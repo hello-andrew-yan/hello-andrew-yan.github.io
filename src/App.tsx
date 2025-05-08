261e9d4c79ed5fd0c1c93a566368d827e17c21f2
Andrew <hello.andrew.yan@outlook.com>
2025-05-08 18:19:40 +1000

    Startup Files for React + Typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from '/cube.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo react" alt="Site Logo" />
+      </div>
+      <h1>Andrew Yan</h1>
+    </>
+  )
+}
+
+export default App
